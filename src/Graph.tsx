e04d47c9ed99478d229922f334d6f7049e283c58
Irfan <irfncr@gmail.com>
2021-03-26 23:26:43 -0500

    Update npm packages

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@jpmorganchase/perspective';
+import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
